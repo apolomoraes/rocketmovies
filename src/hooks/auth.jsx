0070ead3954887dbfedb5ed0e796931e87792aa7
Apolo Moraes <vlogsapolo@gmail.com>
2023-03-17 16:03:26 -0300

    ajuste no header das requisições

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -16,7 +16,8 @@ function AuthProvider({ children }) {
       localStorage.setItem("@rocketnotes:token", token);
 
 
-      api.defaults.headers.authorization = `Bearer ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
       setData({ user, token })
 
     } catch (error) {
@@ -40,7 +41,7 @@ function AuthProvider({ children }) {
     const user = localStorage.getItem("@rocketnotes:user");
 
     if (token && user) {
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
       setData({
         token,
