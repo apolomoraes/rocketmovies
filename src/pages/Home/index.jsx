99ded9842a1144a87bd3dad0bac4d91b997b9c63
Apolo Moraes <vlogsapolo@gmail.com>
2023-03-22 22:47:08 -0300

    adicionando tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,9 +5,19 @@ import { Rating } from "../../components/Rating"
 import { Tags } from "../../components/Tags"
 import { FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from "react"
 
 
 export function Home() {
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags");
+      setTags(response.data)
+    }
+  })
+
   return (
     <Container>
       <Header />
@@ -24,12 +34,18 @@ export function Home() {
             <Rating width={"1.2rem"} height={"1.2rem"} rating={5} />
             <p>Harry Potter é uma série de sete romances de fantasia escrita pela autora britânica J. K. Rowling. A série narra as aventuras de um jovem chamado Harry James Potter, que descobre aos 11 anos de idade que é um bruxo ao ser convidado para estudar na Escola de Magia e Bruxaria de Hogwarts.</p>
             <div className="tag">
-              <Tags title="Harry" padding={".5rem 1.6rem"} background={"#312E38"} />
-              <Tags title="Suspense" padding={".5rem 1.6rem"} background={"#312E38"} />
-              <Tags title="ficção" padding={".5rem 1.6rem"} background={"#312E38"} />
+              {
+                tags && tags.map((tag) => (
+                  <Tags
+                    key={String(tag.id)}
+                    title={tag.name}
+                    padding={".5rem 1.6rem"}
+                    background={"#312E38"}
+                  />
+                ))
+              }
             </div>
           </Section>
-
         </div>
       </Content>
 
